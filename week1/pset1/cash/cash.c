ebeea0ceadc1e0a9d38a85fc4bccb6a4e447224e
tigereye440 <84235364+tigereye440@users.noreply.github.com>
2024-01-24 14:55:00 +0000

    Create cash.c
    Solution for CS50 week1's Cash problem. Passed all of check50 requirements.

---
+++ week1/pset1/cash/cash.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+const int QUARTERS = 25;
+const int DIMES = 10;
+const int NICKELS = 5;
+const int PENNIES = 1;
+
+int coins(int change);
+
+int main(void)
+{
+    int change_owed;
+    do
+    {
+        change_owed = get_int("Enter your change in cents: ");
+    }
+    while (change_owed < 0);
+
+    printf("Coins:  %i\n", coins(change_owed));
+}
+
+int coins(int change)
+{
+    int remainder = 0;
+    int quarters = change / QUARTERS;
+    remainder = change - (quarters * QUARTERS);
+    int dimes = remainder / DIMES;
+    remainder = remainder - (dimes * DIMES);
+    int nickels = remainder / NICKELS;
+    remainder = remainder - (nickels * NICKELS);
+    int pennies = remainder / PENNIES;
+    remainder = remainder - (pennies * PENNIES);
+
+    int coins = quarters + dimes + nickels + pennies;
+    return coins;
+}
